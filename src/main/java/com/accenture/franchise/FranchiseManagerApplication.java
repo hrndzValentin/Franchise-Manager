f1f0cba9dd8470c9f3c113a9d03aed004bc92998
Valentin Sanchez <vaher.246@gmail.com>
2026-04-17 12:22:47 -0500

    first commit

---
+++ src/main/java/com/accenture/franchise/FranchiseManagerApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.franchise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchiseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchiseManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/franchise/FranchiseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.franchise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
